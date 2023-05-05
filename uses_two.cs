dac07436cc3fb9915cd1ac9917670729510eff8a
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-05-05 22:29:52 +0530

    Create uses_two.cs

---
+++ uses_two.cs
@@ -0,0 +1,4 @@
+//creating instance to use the GradTwo class
+GradientBackground gradientBackground = new GradientBackground(Color.Blue, Color.LightBlue);
+//using-->applying
+gradientBackground.ApplyTo(this);
