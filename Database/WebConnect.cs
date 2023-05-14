3621c7d20635386ea551bd8923afc97e69fe3eaf
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-05-14 18:43:48 +0530

    Create WebConnect.cs

---
+++ Database/WebConnect.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Replace your sever details here-->
+        string connectionString = "Data Source=SERVER_NAME;Initial Catalog=DATABASENAME;User ID=USERNAME;Password=PASSWORD;";
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        try
+        {
+            connection.Open();
+            Console.WriteLine("Connection successful.");
+
+            //Data Insertion here--->
+            string insertSql = "INSERT INTO TableName (Column1, Column2) VALUES (@Value1, @Value2)";
+            SqlCommand insertCommand = new SqlCommand(insertSql, connection);
+            insertCommand.Parameters.AddWithValue("@Value1", "Hello");
+            insertCommand.Parameters.AddWithValue("@Value2", "World!");
+            int rowsAffected = insertCommand.ExecuteNonQuery();
+            Console.WriteLine(rowsAffected + " rows inserted.");
+
+           //Data deletation
+            string deleteSql = "DELETE FROM TableName WHERE Column1 = @Value1";
+            SqlCommand deleteCommand = new SqlCommand(deleteSql, connection);
+            deleteCommand.Parameters.AddWithValue("@Value1", "Hello");
+            rowsAffected = deleteCommand.ExecuteNonQuery();
+            Console.WriteLine(rowsAffected + " rows deleted.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Couldn't connect to database: " + ex.Message);
+        }
+        finally
+        {
+            connection.Close();
+        }
+
+        Console.ReadLine();
+    }
+}
