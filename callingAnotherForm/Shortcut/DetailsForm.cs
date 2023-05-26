329a83806281a3aa65b02f7a955a79091930ca75
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-05-26 22:06:09 +0530

    Create DetailsForm.cs

---
+++ callingAnotherForm/Shortcut/DetailsForm.cs
@@ -0,0 +1,26 @@
+//Form I want to keep as secret
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Device_Informations
+{
+    public partial class DetailsForm : Form
+    {
+        public DetailsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DetailsForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
