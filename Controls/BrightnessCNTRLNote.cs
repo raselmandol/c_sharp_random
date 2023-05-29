dffe69bd1f78c75e1519a7e7a2acb94e625ecaa2
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-05-29 23:45:30 +0530

    Create BrightnessCNTRLNote.cs

---
+++ Controls/BrightnessCNTRLNote.cs
@@ -0,0 +1,7 @@
+//Create a new Windows Forms project
+//Add a button control to your main form. Set its name to "brightnessSetupButton" and text to "Brightness Setup".
+//Double-click on the button to generate the click event handler.
+//In the event handler method, create and show a new form that will serve as the popup.
+//Add a TrackBar control to the popup form. This control will be used for brightness adjustment. Set its name to "brightnessTrackBar" and adjust its properties, such as minimum and maximum values, according to your requirements.
+//Add an event handler to the TrackBar control's Scroll event.
+//In the Scroll event handler, retrieve the value of the TrackBar and set the brightness of the main form or the entire screen using appropriate API calls.
