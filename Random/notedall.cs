5003f9cd91b9e3922ba851e4d839fa787bec0691
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-06-03 00:00:31 +0530

    Create notedall.cs

---
+++ Random/notedall.cs
@@ -0,0 +1,4 @@
+//Installed RAM: It calculates the total installed RAM capacity by querying the Win32_PhysicalMemory class.
+//RAM Usage: It uses performance counters to measure the RAM usage and free RAM.
+//Hard Disk Storages: It retrieves information about the available drives, including their names, total size, used space, and free space.
+//Processor Info: It uses the Win32_Processor class to retrieve the processor name and load percentage. Additionally,
