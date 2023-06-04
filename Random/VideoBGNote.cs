5404000ffa1f9a8bf57ee37d9d0b8be1b4b4eb2b
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-06-04 22:32:33 +0530

    Create VideoBGNote.cs

---
+++ Random/VideoBGNote.cs
@@ -0,0 +1,18 @@
+//To set a video as the background of the main form in a WinForms application and play it continuously, you can follow these steps in C#:
+
+//Add a reference to the Windows Media Player COM component:
+
+//Right-click on your project in the Solution Explorer.
+//Select "Add" -> "Reference".
+//Go to the "COM" tab.
+//Find and select "Windows Media Player" and click "OK".
+//Add a Windows Media Player control to your form:
+
+//Open your form in the designer.
+//Go to the Toolbox and find the "Windows Media Player" control.
+//Drag and drop the control onto your form.
+//Set up the Windows Media Player control:
+
+//Select the Windows Media Player control on your form.
+//In the Properties window, set the "uiMode" property to "none" to hide the player controls.
+//Set the "stretchToFit" property to "true" to stretch the video to fill the form.
