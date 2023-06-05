b68b8c3e99f120fe4f6ab4a75fbcc7d4033cee64
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-06-05 23:44:55 +0530

    Create volumeControlNote.cs

---
+++ Random/volumeControlNote.cs
@@ -0,0 +1,6 @@
+//Install the NAudio NuGet package to your project. 
+//You can do this by right-clicking on your project in Visual Studio, selecting "Manage NuGet Packages," searching for "NAudio," and clicking on "Install" to add the package to your project.
+
+//Add two Button controls to your WinForms form. Let's name them buttonIncreaseVolume and buttonDecreaseVolume.
+
+//Update the code in your form's code-behind as shown below:
