ea2af0b3b79c80124d19ff421be8f65b8e6a8bd4
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-06-07 22:02:22 +0530

    Create PrBarColor.cs

---
+++ Random/others/PrBarColor.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ChangeProgressBarColor
+{
+    public partial class Form1 : Form
+    {
+        private PerformanceCounter ramCounter;
+        private const float MaxRamValue = 100f;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeRamCounter();
+            StartRamUsageUpdate();
+        }
+
+        private void InitializeRamCounter()
+        {
+            ramCounter = new PerformanceCounter("Memory", "% Committed Bytes In Use");
+        }
+
+        private void StartRamUsageUpdate()
+        {
+            timerUpdateRamUsage.Interval = 1000;
+            timerUpdateRamUsage.Tick += TimerUpdateRamUsage_Tick;
+            timerUpdateRamUsage.Start();
+        }
+
+        private void TimerUpdateRamUsage_Tick(object sender, EventArgs e)
+        {
+            float ramUsage = ramCounter.NextValue();
+            progressBarRamUsage.Value = (int)ramUsage;
+            UpdateProgressBarColor(ramUsage);
+        }
+
+        private void UpdateProgressBarColor(float ramUsage)
+        {
+            Color color;
+            if (ramUsage <= 50f)
+                color = Color.Green;
+            else if (ramUsage <= 80f)
+                color = Color.Yellow;
+            else
+                color = Color.Red;
+
+            progressBarRamUsage.ForeColor = color;
+        }
+    }
+}
