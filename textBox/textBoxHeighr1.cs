8e52cf6afcb5b60e02d5a358aa97ae526cf060ed
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-06-19 23:52:01 +0530

    Create textBoxHeighr1.cs

---
+++ textBox/textBoxHeighr1.cs
@@ -0,0 +1,13 @@
+       //You will find this in designer.cs, just select your textBox
+       
+       this.mainPromptBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(65)))), ((int)(((byte)(79)))));
+            this.mainPromptBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.mainPromptBox.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainPromptBox.ForeColor = System.Drawing.Color.DimGray;
+            this.mainPromptBox.Location = new System.Drawing.Point(332, -1);
+            this.mainPromptBox.Multiline = true;
+            this.mainPromptBox.Name = "mainPromptBox";
+            //here you make the autosize false that is controlled by the text-font size
+            this.mainPromptBox.AutoSize = false;
+            this.mainPromptBox.Size = new System.Drawing.Size(925, 60);
+            this.mainPromptBox.TabIndex = 0;
