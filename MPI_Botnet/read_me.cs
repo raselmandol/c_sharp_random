3047254495cf47a1b3a802081fdc0b10791e4aca
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-06-30 21:30:25 +0530

    Create read_me.cs

---
+++ MPI_Botnet/read_me.cs
@@ -0,0 +1,3 @@
+//To use MPI in C#, you'll need to install the MPI.NET library, 
+//which provides bindings to the MPI library.
+//You can install it using NuGet by searching for "MPI.NET" in the NuGet Package Manager.
