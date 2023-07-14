09a141ae17c9c93f28bb9f0a4876df292c4e40e5
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-07-14 20:41:06 +0530

    Create webViewControl.cs

---
+++ WebView/webViewControl.cs
@@ -0,0 +1,8 @@
+using Microsoft.Toolkit.Forms.UI.Controls; //For Windows Forms
+//OR
+//using Microsoft.Toolkit.Wpf.UI.Controls; //For WPF
+
+//....//...other codes...//
+
+//Creating a WebView control
+WebView webView = new WebView();
