a893a2662549dad0b986d114ad824bc60afd61ac
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-07-14 20:40:09 +0530

    Create note.cs

---
+++ WebView/note.cs
@@ -0,0 +1,5 @@
+//Add the necessary references to use the WebView control.
+//For Windows Forms, right-click on the "References" node in the Solution Explorer,
+//choose "Add Reference," and select the "Microsoft.Toolkit.Win32.UI.Controls.WinForms" package.
+//For WPF, right-click on the "References" node in the Solution Explorer, choose "Manage NuGet Packages,"
+//search for and install the "Microsoft.Toolkit.Wpf.UI.Controls.WebView" package.
