02e90cf16ae81bb5d62d662ca2e2131bcacb5a05
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-07-15 23:42:25 +0530

    Create note.cs

---
+++ webScraping/note.cs
@@ -0,0 +1,6 @@
+//Make sure to replace the url variable with the actual URL of the website you want to crawl.
+//This code uses the HttpClient class to send an HTTP GET request to the specified URL.
+//It then reads the response content as a string and prints it to the console.
+
+//Note that web scraping/crawling may have legal and ethical implications, 
+//so ensure that you have the necessary permissions to crawl the website and follow the website's terms of service.
